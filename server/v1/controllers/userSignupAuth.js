9a4eb4318ad37e06fdb53157c714219c8f3e894a
regiss <regiss.steven@gmail.com>
2019-09-06 10:23:23 +0200

    ft(endpoint): accept and denied git push origin ft-acc-decl-endpoint-168285248 mentor can accept or decline mentorship request [finishes #168285248,#168285305]

--- server/v1/controllers/userSignupAuth.js
+++ server/v1/controllers/userSignupAuth.js
@@ -8,34 +8,28 @@ const signup = (req, res) => {
     password, 
   } = req.body;
   
-  if(firstName && lastName && email && password){
 
-    const myuser = new user(
-      userObj.length, 
-      email,
-      firstName,
-      lastName,
-      password,
-    );
+  const myuser = new user(
+    userObj.length, 
+    email,
+    firstName,
+    lastName,
+    password,
+  );
 
-    userObj.push( myuser);
-    if(myuser){
-      res.status(201).send({
-        message: 'User created successfully',
-        data: {
-          userId: myuser.userId,
-          email: myuser.email,
-          token: myuser.token
-        }
-      })
-    }else{
-      res.status(409).send({
-        message:'not created',
-      })
-    }
-  } else {
-    res.status(404).send({
-      message: 'complete all field',
+  userObj.push( myuser);
+  if(myuser){
+    res.status(201).send({
+      message: 'User created successfully',
+      data: {
+        userId: myuser.userId,
+        email: myuser.email,
+        token: myuser.token
+      }
+    })
+  }else{
+    res.status(409).send({
+      message:'not created',
     })
   }
 }
