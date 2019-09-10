f3863d3089b0d79548490a562fde77f555cdddef
regiss05 <regiss.steven@gmail.com>
2019-09-10 11:06:16 -0700

    ft(endpoint): configuration of port [finishes #168377964]

--- server/v2/models/user.js
+++ server/v2/models/user.js
@@ -1,27 +1,27 @@
-import createToken from '../helpers/createToken';
-import pool from '../config/dbconfig';
+// import createToken from '../helpers/createToken';
+// import pool from '../config/dbconfig';
 
 
-export class user {
-  async save(user){
-    const {userName, password, email, isMentor} = user;
+// export class user {
+//   async save(user){
+//     const {userName, password, email, isMentor} = user;
 
-    const queryString = {
-      text: `INSERT INTO users (userName, password, email, isMentor)
-      VALUES($1, $2, $3, $4) RETURNING*;`
-      values: [userName, password, email, isMentor]
-    };
-    const { rows } = await pool.query(queryString);
-    return rows
-  }
-}
+//     const queryString = {
+//       text: `INSERT INTO users (userName, password, email, isMentor)
+//       VALUES($1, $2, $3, $4) RETURNING*;`
+//       values: [userName, password, email, isMentor]
+//     };
+//     const { rows } = await pool.query(queryString);
+//     return rows
+//   }
+// }
 
-const obj = new user(
-  1,
-  'lenovo@lemoisson.com',
-  'lenovo',
-  'thinkpad',
-  'rec',
-)
+// const obj = new user(
+//   1,
+//   'lenovo@lemoisson.com',
+//   'lenovo',
+//   'thinkpad',
+//   'rec',
+// )
 
-export const userObj = [obj];
+// export const userObj = [obj];
