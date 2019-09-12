64d206c3deac52397940d9816265bc8482babe44
regiss05 <regiss.steven@gmail.com>
2019-09-12 08:31:02 -0700

    ft(endpoint): mentor side  mentor should accept or reject mentorship [finishes #168377629, #168377706]

--- server/app.js
+++ server/app.js
@@ -2,9 +2,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import router from './v1/routes/route';
 import routerV2 from './v2/routes/route';
-import Portconfig from './v1/config/PortConfig';
 import dotenv from 'dotenv';
 import pool from '../server/v2/config/dbConfig'
 import {systemAdminDB} from '../server/v2/models/queries'
@@ -15,7 +13,7 @@ const app = express();
 // eslint-disable-next-line camelcase
 
 // port configuration imported
-const { port } = Portconfig;
+const  port  = process.env.PORT || 4500;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,7 +21,6 @@ app.use(bodyParser.json());
 
 
 // API Routes initialized
-app.use('/api/v1/', router);
 app.use('/api/v2/', routerV2);
 
 app.get('/', (req, res) => res.status(200).send({
@@ -46,9 +43,8 @@ app.listen(port, () => {
 });
 
 pool.query(systemAdminDB).then(res =>{
-  console.log(res);
+ 
 }).catch(err =>{
-  console.log(err);
 });
 
 export default app;
